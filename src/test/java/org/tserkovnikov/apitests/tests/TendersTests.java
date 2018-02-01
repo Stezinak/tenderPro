543918cc5b7149742c1bceca79f9719f048ee4d7
Stan Tompson <stan.tompson@bbconsult.co.uk>
2018-02-01 16:19:46 +0400

    some changes for api tests

--- src/test/java/org/tserkovnikov/apitests/tests/TendersTests.java
+++ src/test/java/org/tserkovnikov/apitests/tests/TendersTests.java
@@ -5,14 +5,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.tserkovnikov.apitests.apicommands.TendersAPI;
 
-public class TendersTests {
+public class TendersTests extends TendersAPI{
 
     @Test
     public void GetThreeTenders() {
-        TendersAPI api = new TendersAPI(); // это, конечно, костыль.
-        JsonPath result = api.getFirstThreeTenders("2953");
-        Assert.assertEquals("3", result.get("result.args.max_rows")); // проверяем, что наш запрос вернул только 3 записи
-        Assert.assertEquals("КАМАЗ", result.get("result.data[0].company_name")); // проверяем название фирмы, которая огранизует тендер
+        JsonPath result = getFirstThreeTenders("2953");
+        Assert.assertEquals(result.get("result.args.max_rows"), "3"); // проверяем, что наш запрос вернул только 3 записи
+        Assert.assertEquals(result.get("result.data[0].company_name"), "КАМАЗ"); // проверяем название фирмы, которая огранизует тендер
         // очевидно, что имея json мы можем проверить любые параметры
     }
 }

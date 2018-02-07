2a4f2fdf7f80d4aa85cbfa09ceb4da73567895a6
Stan Tompson <stan.tompson@bbconsult.co.uk>
2018-02-07 21:15:51 +0400

    delete unused imports

--- src/test/java/org/tserkovnikov/uitests/tests/BaseTest.java
+++ src/test/java/org/tserkovnikov/uitests/tests/BaseTest.java
@@ -2,9 +2,7 @@ package org.tserkovnikov.uitests.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.tserkovnikov.uitests.helpers.TestConstants;
 import org.tserkovnikov.uitests.pageobject.LoginPage;
 import org.tserkovnikov.uitests.pageobject.TestContext;
